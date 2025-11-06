998a8efaeff389e6f431cdf01f375358682e26e8
amanverma9008003 <amanverma0039008@gmail.com>
2025-11-06 11:11:59 +0530

    trying again by updating

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 import cors from "cors";
 import 'dotenv/config';
 import router from "./routes/chat.js";
+
 const app = express();
+const PORT=5000;
+
 app.use(express.json());
 app.use(cors());
 app.use("/api",router);
@@ -17,7 +20,6 @@ const connectDB=async ()=>{
     }
 }
 
-const PORT=5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     connectDB();
